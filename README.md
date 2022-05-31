92d5ea42df07be229527146af407b6f3234cf596
César Damasceno <mirandacesar598@gmail.com>
2022-05-30 23:08:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![image](https://user-images.githubusercontent.com/64749161/171078663-48ea329a-69da-4c7d-ac53-d680352f29ee.png)
+
+## About
+
+s30 service presentation single page
---
+++ styles/globalConfig.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/poppins"
+
+const theme = extendTheme({
+  fonts: {
+    heading: `"Poppins", sans- serif`,
+    body: `"Poppins", sans- serif`,
+  },
+})
+
+export default theme
