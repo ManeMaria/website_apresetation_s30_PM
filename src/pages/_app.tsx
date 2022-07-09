0bbfbd07ba5ef629dde3b39b7378c55f874cce16
César Damasceno <mirandacesar598@gmail.com>
2022-07-08 23:27:52 -0300

    refactor: pixel facebook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../../styles/globalConfig';
-import { FacebookPixel } from '../components/Pixel/facebook';
+
 import { DefaultSeo } from 'next-seo';
+import { FacebookPixel } from '@/components/Pixel/facebook';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
