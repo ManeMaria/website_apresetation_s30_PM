14b72ef01862618a55c9da9d7b58fef4f3e504d9
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2022-08-29 23:00:30 -0300

    feat: add url payment

--- src/components/common/ButtonDefault/index.tsx
+++ src/components/common/ButtonDefault/index.tsx
@@ -1,11 +1,16 @@
-import { Button, ButtonGroupProps, ButtonProps, ChakraProps } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Button,
+  ButtonGroupProps,
+  ButtonProps,
+  ChakraProps,
+} from "@chakra-ui/react";
+import React from "react";
 
 export function ButtonDefault({ children, ...props }: ButtonProps) {
   return (
     <Button
       as="a"
-      href="https://pay.hotmart.com/C70709392A?bid=1655479282140"
+      href="https://pay.hotmart.com/C70709392A?off=8zt629zq"
       textStyle="h2"
       color="white"
       bg="yellow.500"
@@ -15,12 +20,12 @@ export function ButtonDefault({ children, ...props }: ButtonProps) {
       fontSize="1.2rem"
       boderColor="yellow.500"
       _hover={{
-        bg: 'yellow.500',
-        boxShadow: '0 0 10px 1px #EDA91E',
+        bg: "yellow.500",
+        boxShadow: "0 0 10px 1px #EDA91E",
       }}
       {...props}
     >
-      {children ?? 'quero ter resultados'}
+      {children ?? "quero ter resultados"}
     </Button>
   );
 }
